67a0205e25efa93571dda12ca05aca7aecd2c3d7
XProtocol <tyagiaviral@gmail.com>
2023-12-19 23:28:49 +0530

    adds answer for middelwares

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -16,6 +16,23 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+function rateLimitter(req, res, next) {
+  const userId = req.headers["user-id"];
+
+  if(numberOfRequestsForUser[userId] > 5) {
+    res.status(404).send("404");
+  }
+  else if(!numberOfRequestsForUser[userId]) {
+    numberOfRequestsForUser[userId] = 1;
+  }
+  else {
+    numberOfRequestsForUser[userId]++;
+  }
+  next();
+}
+
+app.use(rateLimitter);
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
