aff396e3c597ada04fa730966782dea8acd0fae8
XProtocol <tyagiaviral@gmail.com>
2024-01-04 23:34:56 +0530

    adds answer for 03-mongo

--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -1,18 +1,36 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
 const router = Router();
+const { Admin, Course } = require('../db');
 
 // Admin Routes
-router.post('/signup', (req, res) => {
-    // Implement admin signup logic
+router.post('/signup', async (req, res) => {
+    await Admin.create({
+        username : req.body.username,
+        password: req.body.password
+    });
+    res.json({
+        message: 'Admin account created!'
+    });
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
-    // Implement course creation logic
+router.post('/courses', adminMiddleware, async (req, res) => {
+    const newCourse = await Course.create({
+        title : req.body.title,
+        description : req.body.description,
+        imageLink : req.body.imageLink,
+        price : req.body.price
+    });
+    res.json({
+        message: 'Course created!', courseId: newCourse._id
+    });
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
-    // Implement fetching all courses logic
+router.get('/courses', adminMiddleware, async (req, res) => {
+    const allCourses = await Course.find({});
+    res.json({
+        courses: allCourses
+    });
 });
 
 module.exports = router;
